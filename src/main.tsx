e31c4bd692da4e9d6913bf9203e6687896d9d67c
Tovol Ron <tovolron@gmail.com>
2026-03-31 04:07:15 +0800

    added google authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 
   import { createRoot } from "react-dom/client";
+  import { GoogleOAuthProvider } from "@react-oauth/google";
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <GoogleOAuthProvider clientId="322543435047-avhj92akciptrms4sd6sqju7ipr75ru8.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  );
   
